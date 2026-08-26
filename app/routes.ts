398e911d5a897a230e483a7b27617093f3bfde13
qblanchard <33019000+qblanchard@users.noreply.github.com>
2026-08-26 23:23:44 +0200

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig,route, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
